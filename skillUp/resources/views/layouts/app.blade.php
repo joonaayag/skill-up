59451245cea8a5e63b77628c330f1ef80e0a0d46
Jonay <joonaayag19@gmail.com>
2025-05-01 11:01:22 +0200

    Vite instalation and config data for run

--- skillUp/resources/views/layouts/app.blade.php
+++ skillUp/resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'Dashboard')</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
     @include('partials.header')
--- skillUp/resources/views/welcome.blade.php
+++ skillUp/resources/views/welcome.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>SkillUp</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>Bienvenido a la plataforma</h1>
+    <h1 class="bg-pink-900">Bienvenido a la plataforma</h1>
     <a href="{{ route('auth') }}">Iniciar sesión o registrarse</a>
 </body>
 </html>
