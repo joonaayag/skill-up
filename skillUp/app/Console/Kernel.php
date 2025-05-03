8e09510c2fdd0e77b94805cd44d96f66856eb3e2
Jonay <joonaayag19@gmail.com>
2025-05-03 19:44:40 +0200

    Added auto delete old notifications

--- skillUp/app/Console/Kernel.php
+++ skillUp/app/Console/Kernel.php
@@ -8,17 +8,20 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        \App\Console\Commands\EnviarSugerenciasUsuarios::class,
+        EnviarSugerenciasUsuarios::class,
+        LimpiarNotificacionesAntiguas::class,
+
     ];
 
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('notificaciones:limpiar')->daily();  
         $schedule->command('notificaciones:sugerencias')->daily();
     }
 
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
