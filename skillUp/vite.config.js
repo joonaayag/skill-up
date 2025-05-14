f3fe80cf2715bc6ad9691477fabfb2fb994955f7
Jonay <joonaayag19@gmail.com>
2025-05-14 10:06:08 +0200

    Modified viteconfig trying to fix styles bug

--- skillUp/vite.config.js
+++ skillUp/vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            url: process.env.APP_URL || 'https://skill-up-production-7fdb.up.railway.app',
+            buildDirectory: 'build',
         }),
         tailwindcss(),
     ],
+    server: {
+        https: true,
+    },
 });
