3bfd355fb425dbd5a01e1a72c8f75339de43c470
Jonay <joonaayag19@gmail.com>
2025-05-14 10:22:16 +0200

    Modified viteconfig trying to fix styles bug

--- skillUp/resources/views/layouts/app.blade.php
+++ skillUp/resources/views/layouts/app.blade.php
@@ -10,10 +10,9 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Dashboard')</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-           <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    @endif
 </head>
 
 <body class="bg-no-repeat bg-cover bg-center flex flex-col min-h-screen transition-all duration-500 text-themeBgDark dark:text-white"
