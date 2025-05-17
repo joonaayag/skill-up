e644381eb3eb67d545b62dc0fbf222f53f6a4067
Jonay <joonaayag19@gmail.com>
2025-05-17 14:56:39 +0200

    Application management translated

--- skillUp/resources/views/applications/index.blade.php
+++ skillUp/resources/views/applications/index.blade.php
@@ -3,31 +3,31 @@
 @section('title', 'Gestión de candidaturas')
 
 @section('content')
-    <x-heading level="h1" class="mb-10">Gestión de candidaturas</x-heading>
+    <x-heading level="h1" class="mb-10">{{ __('messages.applications.title') }}</x-heading>
 
     <div class="grid grid-cols-4 gap-24 ">
         <x-card class="h-full col-span-1">
             <form id="candidate-filter-form" method="GET" action="{{ route('applications.index') }}"
                 class=" flex flex-col [&>select]:h-12 [&>select]:bg-white dark:[&>select]:bg-themeBgDark [&>select]:rounded-lg [&>select]:border-2 [&>select]:border-themeLightGray [&>select]:px-4 ">
 
-                <x-heading level="h3" class="mb-2.5">Puesto</x-heading>
+                <x-heading level="h3" class="mb-2.5">{{ __('messages.applications.position') }}</x-heading>
 
                 <x-inputtext class="mb-3" type="text" name="position_applied" id="position_applied"
-                    value="{{ request('position_applied') }}" placeholder="Buscar por puesto" required />
+                    value="{{ request('position_applied') }}" placeholder="{{ __('messages.applications.placeholder-position') }}" required />
 
-                <x-heading level="h3" class="mb-2.5">Estado</x-heading>
+                <x-heading level="h3" class="mb-2.5">{{ __('messages.applications.status') }}</x-heading>
                 <select class="mb-3" name="state">
-                    <option value="">Estado</option>
-                    <option value="nueva" @selected(request('state') == 'nueva')>Nueva</option>
-                    <option value="en revisión" @selected(request('state') == 'en revisión')>En revisión</option>
-                    <option value="aceptado" @selected(request('state') == 'aceptado')>Aceptado</option>
-                    <option value="rechazado" @selected(request('state') == 'rechazado')>Rechazado</option>
+                    <option value="">{{ __('messages.applications.status') }}</option>
+                    <option value="nueva" @selected(request('state') == 'nueva')>{{ __('messages.applications.new') }}</option>
+                    <option value="en revisión" @selected(request('state') == 'en revisión')>{{ __('messages.applications.in-process') }}</option>
+                    <option value="aceptado" @selected(request('state') == 'aceptado')>{{ __('messages.applications.accepted') }}</option>
+                    <option value="rechazado" @selected(request('state') == 'rechazado')>{{ __('messages.applications.rejected') }}</option>
                 </select>
 
-                <x-heading level="h3" class="mb-2.5">Nombre</x-heading>
+                <x-heading level="h3" class="mb-2.5">{{ __('messages.applications.name') }}</x-heading>
 
                 <x-inputtext type="text" name="candidate_name" id="candidate_name" value="{{ request('candidate_name') }}"
-                    placeholder="Nombre del candidato" required />
+                    placeholder="{{ __('messages.applications.placeholder-name') }}" required />
 
             </form>
         </x-card>
@@ -35,10 +35,10 @@
             <table class="w-full text-left">
                 <thead>
                     <tr class="border-b">
-                        <th class="py-2">Nombre</th>
-                        <th class="py-2">Estado</th>
-                        <th class="py-2">Fecha de realización</th>
-                        <th class="py-2">Acciones</th>
+                        <th class="py-2">{{ __('messages.applications.table-name') }}</th>
+                        <th class="py-2">{{ __('messages.applications.table-status') }}</th>
+                        <th class="py-2">{{ __('messages.applications.table-date') }}</th>
+                        <th class="py-2">{{ __('messages.applications.table-actions') }}</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -56,56 +56,54 @@
                                                                                                             $watch('showDetails', val => document.body.classList.toggle('overflow-hidden', val));">
                                 <button @click="showDetails = true"
                                     class="p-2 bg-themeBlue text-white shadow-lg rounded-lg cursor-pointer hover:bg-themeHoverBlue/80 transition">
-                                    Ver
+                                    {{ __('messages.buttons.see') }}
                                 </button>
 
                                 <x-modal class="flex items-center justify-center" :show="'showDetails'">
 
-                                    <x-heading level="h2" class="mb-4 text-center pb-4 border-b-2 border-b-themeBlue">Detalles
-                                        de la candidatura de {{ $app->candidate_name }}
+                                    <x-heading level="h2" class="mb-4 text-center pb-4 border-b-2 border-b-themeBlue">{{ __('messages.applications.candidate-details') }} {{ $app->candidate_name }}
                                     </x-heading>
                                     <div class="flex flex-col items-center text-left gap-2.5 mb-4 ">
-                                        <p><strong>Nombre:</strong> {{ $app->candidate_name }}</p>
-                                        <p><strong>Puesto solicitado:</strong> {{ $app->position_applied }}</p>
-                                        <p><strong>Motivo:</strong> {{ $app->application_reason }}</p>
-                                        <p><strong>Fecha:</strong> {{ $app->application_date }}</p>
+                                        <p><strong>{{ __('messages.applications.modal-name') }}</strong> {{ $app->candidate_name }}</p>
+                                        <p><strong>{{ __('messages.applications.modal-position') }}</strong> {{ $app->position_applied }}</p>
+                                        <p><strong>{{ __('messages.applications.modal-reason') }}:</strong> {{ $app->application_reason }}</p>
+                                        <p><strong>{{ __('messages.applications.modal-date') }}</strong> {{ $app->application_date }}</p>
 
                                         @if ($app->cv)
                                             <p>
                                                 <a href="{{ asset('storage/' . $app->cv) }}" class="text-blue-600 underline"
                                                     target="_blank">📄
-                                                    Ver CV</a>
+                                                    {{ __('messages.applications.see-cv') }}</a>
                                             </p>
                                         @else
-                                            <p class="text-gray-500">No se subió CV</p>
+                                            <p class="text-gray-500">{{ __('messages.applications.no-cv') }}</p>
                                         @endif
 
                                         <form method="POST" action="{{ route('applications.update', $app->id) }}">
                                             @csrf
                                             @method('PUT')
 
-                                            <label for="state">Cambiar estado:</label>
+                                            <label for="state">{{ __('messages.applications.change-status') }}</label>
                                             <select name="state" required class="dark:bg-themeBgDark">
-                                                <option value="nueva" {{ $app->state === 'nueva' ? 'selected' : '' }}>Nueva
+                                                <option value="nueva" {{ $app->state === 'nueva' ? 'selected' : '' }}>{{ __('messages.applications.new') }}
                                                 </option>
                                                 <option value="en revisión" {{ $app->state === 'en revisión' ? 'selected' : '' }}>
-                                                    En
-                                                    revisión
+                                                    {{ __('messages.applications.in-process') }}
                                                 </option>
                                                 <option value="aceptado" {{ $app->state === 'aceptado' ? 'selected' : '' }}>
-                                                    Aceptado
+                                                    {{ __('messages.applications.accepted') }}
                                                 </option>
                                                 <option value="rechazado" {{ $app->state === 'rechazado' ? 'selected' : '' }}>
-                                                    Rechazado
+                                                    {{ __('messages.applications.rejected') }}
                                                 </option>
                                             </select>
                                             <br>
                                         </form>
                                         <div>
                                             <button type="submit"
-                                                class="px-4 py-2 bg-themeBlue text-gray-800 cursor-pointer hover:bg-themeHoverBlue/80 transition rounded-lg">Guardar</button>
+                                                class="px-4 py-2 bg-themeBlue text-gray-800 cursor-pointer hover:bg-themeHoverBlue/80 transition rounded-lg">{{ __('messages.button.save-changes') }}</button>
                                             <button @click="showDetails = false"
-                                                class="px-4 py-2 bg-themeLightGray/50 text-gray-800 cursor-pointer hover:bg-themeLightGray transition rounded-lg">Cerrar</button>
+                                                class="px-4 py-2 bg-themeLightGray/50 text-gray-800 cursor-pointer hover:bg-themeLightGray transition rounded-lg">{{ __('messages.button.cancel') }}</button>
                                         </div>
 
                                     </div>
@@ -113,21 +111,20 @@
 
                                 <button @click="showDelete = true"
                                     class="p-2 bg-white dark:bg-themeBgDark text-themeRed border-2 border-themeRed shadow-lg rounded-lg hover:bg-themeHoverRed/30 transition cursor-pointer">
-                                    Eliminar
+                                    {{ __('messages.button.delete') }}
                                 </button>
 
                                 <x-modal :show="'showDelete'">
-                                    <x-heading level="h3" class="mb-4 text-center pb-4 border-b-2 border-b-themeBlue">Seguro
-                                        deses eliminar a {{ $app->candidate_name }} de la oferta {{ $app->position_applied }}?</x-heading>
+                                    <x-heading level="h3" class="mb-4 text-center pb-4 border-b-2 border-b-themeBlue">{{ __('messages.applications.delete-confirm-1') }} {{ $app->candidate_name }} {{ __('messages.applications.delete-confirm-2') }} {{ $app->position_applied }}?</x-heading>
                                     <form action="{{ route('applications.destroy', $app->id) }}" method="POST"
                                         class="flex justify-center gap-3">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit"
-                                            class="p-2 bg-white dark:bg-themeBgDark text-themeRed border-2 border-themeRed shadow-lg rounded-lg hover:bg-themeHoverRed/30 transition cursor-pointer">Eliminar</button>
+                                            class="p-2 bg-white dark:bg-themeBgDark text-themeRed border-2 border-themeRed shadow-lg rounded-lg hover:bg-themeHoverRed/30 transition cursor-pointer">{{ __('messages.applications.delete') }}</button>
                                         <button type="button"
                                             class="px-4 py-2 bg-themeLightGray text-gray-800 cursor-pointer hover:bg-gray-300 transition rounded-lg"
-                                            @click="showDelete = false">Cancelar</button>
+                                            @click="showDelete = false">{{ __('messages.button.cancel') }}</button>
                                     </form>
 
                                 </x-modal>
@@ -136,7 +133,7 @@
                     @empty
                         <tr>
                             <td colspan="4" class="text-center py-4">
-                                <p class="text-lg">No hay candidaturas</p>
+                                <p class="text-lg">{{ __('messages.applications.no-applications') }}</p>
                             </td>
                         </tr>
                     @endforelse
