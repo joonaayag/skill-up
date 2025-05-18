e5fb5d413c4de81f4aa211df31a3c04084e90137
Jonay <joonaayag19@gmail.com>
2025-05-18 12:29:15 +0200

    Styles in ajax added

--- skillUp/app/Http/Controllers/NotificationController.php
+++ skillUp/app/Http/Controllers/NotificationController.php
@@ -19,17 +19,20 @@ class NotificationController extends Controller
 
         foreach (auth()->user()->notifications()->latest()->take(4)->get() as $notification) {
             $dashboardHtml .= '
-            <div class="flex items-center space-x-4 leading-card mb-2.5 hover:bg-themeLightGray/20 cursor-pointer p-1 rounded-lg transition">
-                <div class="bg-themeGrape text-white p-2 rounded-full">
-                    <svg class="w-8 h-auto" fill="none" stroke="currentColor"><use href="#icon-bell"></use></svg>
-                </div>
-                <div class="[&>p]:mt-1">
-                    <p class="font-semibold">' . e($notification->message) . '</p>
-                    <p class="text-xs text-themeSmallTextLightGray dark:text-darkThemeSmallTextLightGray">'
-                . $notification->created_at->diffForHumans() .
-                '</p>
-                </div>
-            </div>';
+            <h3 class="text-2xl font-semibold mb-8">'
+        . __('messages.dashboard.recent-notifications') .'</h3>
+            <div
+            class="flex items-center space-x-4 leading-card mb-2.5 hover:bg-themeLightGray/20 cursor-pointer p-1 rounded-lg transition">
+            <div class="bg-themeGrape text-white p-2 rounded-full">
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-bell"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /></svg>
+            </div>
+            <div class="[&>p]:mt-1">
+                <p class="font-semibold">' . $notification->message . '</p>
+                <p class="text-xs text-themeSmallTextLightGray dark:text-darkThemeSmallTextLightGray">
+                    ' . $notification->created_at->diffForHumans() . '
+                </p>
+            </div>
+        </div>';
         }
 
         if ($notificationCount === 0) {
