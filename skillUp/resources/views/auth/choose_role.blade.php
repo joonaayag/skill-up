aa074ec8c955affb330de6e421d3007c34b8cfdf
Jonay <joonaayag19@gmail.com>
2025-06-02 15:50:16 +0200

    Miss letter

--- skillUp/resources/views/auth/choose_role.blade.php
+++ skillUp/resources/views/auth/choose_role.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Skill Up - {{ __('messages.titles.last-step') }}</title>
+    <title>Skill Up - {{ __('messages.titles.last-steps') }}</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <link rel="icon" type="image/x-icon" href="{{ asset('icons/logo.svg') }}">
